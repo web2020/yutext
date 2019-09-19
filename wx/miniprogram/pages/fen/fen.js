8a8a4c2ea24c546a730ee6bba70ec5fa8d355561
yu <2019288395@qq.com>
2019-09-19 16:04:47 +0800

    888

---
+++ wx/miniprogram/pages/fen/fen.js
@@ -0,0 +1,166 @@
+// pages/fen/fen.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    lists: [{
+        id: 1,
+        img: ("../../images/01.jpg"),
+        price:38.99
+      },
+      {
+        id: 2,
+        img: ("../../images/02.jpeg"),
+        price: 20.99
+      },
+      {
+        id: 3,
+        img: ("../../images/03.jpg"),
+        price: 18.99
+      },
+      {
+        id: 4,
+        img: ("../../images/04.jpg"),
+        price: 17.99
+      },
+      {
+        id: 5,
+        img: ("../../images/05.jpg"),
+        price: 28.99
+      },
+      {
+        id: 6,
+        img: ("../../images/06.jpg"),
+        price: 18.99
+      },
+      {
+        id: 7,
+        img: ("../../images/07.jpg"),
+        price: 28.99
+      },
+      {
+        id: 8,
+        img: ("../../images/08.jpg"),
+        price: 19.99
+      },
+      {
+        id: 9,
+        img: ("../../images/09.jpg"),
+        price: 22.99
+      },
+      {
+        id: 10,
+        img: ("../../images/010.jpg"),
+        price: 26.99
+      },
+      {
+        id: 11,
+        img: ("../../images/011.jpg"),
+        price: 36.99
+      },
+      {
+        id: 12,
+        img: ("../../images/012.jpg"),
+        price: 55.99
+      },
+      {
+        id: 13,
+        img: ("../../images/013.jpg"),
+        price: 77.99
+      },
+      {
+        id: 14,
+        img: ("../../images/014.jpeg"),
+        price: 66.99
+      },
+      {
+        id: 15,
+        img: ("../../images/015.jpg"),
+        price: 44.99
+      },
+      {
+        id: 16,
+        img: ("../../images/016.jpg"),
+        price: 33.99
+      },
+      {
+        id: 17,
+        img: ("../../images/017.jpg"),
+        price: 55.99
+      },
+      {
+        id: 18,
+        img: ("../../images/018.jpeg"),
+        price: 99.99
+      },
+      {
+        id: 19,
+        img: ("../../images/019.jpeg"),
+        price: 44.99
+      },
+      {
+        id: 20,
+        img: ("../../images/020.jpg"),
+        price: 22.99
+      },
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  }
+})
--- wx/miniprogram/pages/shop/shop.js
+++ wx/miniprogram/pages/shop/shop.js
@@ -5,62 +5,108 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    lists: [{
+      id: 1,
+      img: ("../../images/hao1.jpeg"),
+      title: "可爱三只小狗，好吃又好玩",
+      n: "可爱三只小狗，好吃又好玩可爱三只小狗，好吃又好玩"
+    }, {
+      id: 2,
+      title: "培根玉米色拉面包，咸味的更喜欢",
+      img: ("../../images/hao2.jpeg"),
+      n: "去皮紫薯 去皮山药 牛奶 蜂蜜 "
+    }, {
+      id: 3,
+      title: "#秋天怎么吃# 川贝雪梨猪骨汤",
+      img: ("../../images/hao3.jpeg"),
+      n: "梭子蟹 蒜 姜片 小葱 黄豆酱"
+    }, {
+      id: 4,
+      title: "蒜子豆酱焗鱼",
+      img: ("../../images/hao4.jpeg"),
+      n: "梭子蟹 蒜 姜片 小葱 黄豆酱 "
+    }, {
+      id: 5,
+      title: "香煎土豆小饼",
+      img: ("../../images/hao5.jpeg"),
+      n: "土豆 面粉 鸡蛋 绿尖椒圈 红椒圈 香菜叶 盐 黑胡椒粉 "
+    }, {
+      id: 6,
+      title: "蒜子豆酱焗蟹",
+      img: ("../../images/hao6.jpg"),
+      n: "梭子蟹 蒜 姜片 小葱 黄豆酱"
+    }],
+    
+   
   },
-
+  jump: function (event) {
+    var id = event.target.dataset.id;
+    wx.navigateTo({
+      url: '/pages/fen/fen?id=' + id,
+    })
+  },
+  vip: function (event) {
+    var id = event.target.dataset.id;
+    wx.navigateTo({
+      url: '/pages/vip/vip?id=' + id,
+    })
+  },
+  /* 页面跳转 wx.navigateTo({
+    url: 'page/home/home?user_id=111'
+  })*/
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
